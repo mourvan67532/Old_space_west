cc317e86d96767fa758e42f68328a21f58a498e2
mourvan <124926548+mourvan67532@users.noreply.github.com>
2024-06-17 12:12:49 -0300

    Add files via upload

--- RPG/index.py
+++ RPG/index.py
@@ -1,48 +1,5 @@
 import csv
 
-#todas as aventuras
-def todas_av(): #função
-    a = [] #variavel
-    with open('csv/old_space_west.csv', 'r', encoding='utf-8') as pla: #abrir o csv
-         m = csv.DictReader(pla) 
-         for i in m: #colocar o csv dentro da variavel
-            a.append(i)
-
-    for i in a: #imprimir as aventuras
-        print(i['Av'])
-
-todas_av() #função
-
-#aventura especifica
-def av_espe():
-    a = []
-    c = int(input("De 0 a 39 qual aventura você deseja abrir: "))
-    with open('csv/old_space_west.csv', 'r', encoding='utf-8') as pla: #abrir o csv
-         m = csv.DictReader(pla) 
-         for i in m: #colocar o csv dentro da variavel
-            a.append(i)
-    print(a[c]['Av'])
-av_espe() #função
-
-
-
-
-
-#apaga elemento do vetor
-def apaga_ele_v(): #função
-    a = [] #variavel
-    c = int(input("De 0 a 39 qual aventura você deseja apagar: ")) #numero da aventura a ser apagada
-    with open('csv/old_space_west.csv', 'r', encoding='utf-8') as pla: #abrir o csv
-         m = csv.DictReader(pla) 
-         for i in m: #colocar o csv dentro da variavel
-            a.append(i)
-    del a[c]['Av'] #apagar a aventura
-    print(a) #imprimi vetor com a aventura apagada
-    
-
-apaga_ele_v() #função
-
-
 #continua
 def cont(): #função
     copia = input("Digite y para continuar: ") #input
@@ -50,5 +7,55 @@ def cont(): #função
        copia = input("Digite y para continuar: ") #repetir input se não for Y ou y
 
     print("Vocé continuou com sucesso") #imprimir mensagem
+
+
+#apaga elemento do vetor
+def apaga_ele_v(b): #função
+    c = int(input("De 0 a 39 qual aventura você deseja apagar: ")) #numero da aventura a ser apagada
+    del b[c]['Av'] #apagar a aventura
+    print(a) #imprimi vetor com a aventura apagada
+    
+
+#aventura especifica
+def av_espe(b):
+    c = int(input("De 0 a 39 qual aventura você deseja abrir: "))
+    print(b[c]['Av'])
+
+
+#todas as aventuras
+def todas_av(b): #função
+    for i in b: #imprimir as aventuras
+        print(i['Av'])
+
+with open('txt/rpg_texto.txt') as texto:
+    ler_texto = texto.read()
+print(ler_texto)
+
+cont()
+print("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
+
+a = [] #variavel
+with open('csv/old_space_west.csv', 'r', encoding='utf-8') as pla: #abrir o csv
+    m = csv.DictReader(pla) 
+    for i in m: #colocar o csv dentro da variavel
+        a.append(i)
+
+
+
+
+todas_av(a) #função
+
+
+
+
+av_espe(a) #função
+
+
+
+
+apaga_ele_v(a) #função
+
+
+
 
 cont() #função
